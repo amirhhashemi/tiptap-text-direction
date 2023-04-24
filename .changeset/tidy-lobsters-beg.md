21ed65cf47f2970c07cdb4d52e7dd19efeee83cd
amirhhashemi <87268103+amirhhashemi@users.noreply.github.com>
2023-04-24 13:11:38 +0330

    fix!: don't export "RTL_REGEX" and "LTR_REGEX"

---
+++ .changeset/tidy-lobsters-beg.md
@@ -0,0 +1,5 @@
+---
+"tiptap-text-direction": patch
+---
+
+BREAKING CHANGE: Don't export "RTL_REGEX" and "LTR_REGEX"
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const LTR =
   "\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u200E\u2C00-\uFB1C" +
   "\uFE00-\uFE6F\uFEFD-\uFFFF";
 
-export const RTL_REGEX = new RegExp("^[^" + LTR + "]*[" + RTL + "]");
-export const LTR_REGEX = new RegExp("^[^" + RTL + "]*[" + LTR + "]");
+const RTL_REGEX = new RegExp("^[^" + LTR + "]*[" + RTL + "]");
+const LTR_REGEX = new RegExp("^[^" + RTL + "]*[" + LTR + "]");
 
 type Direction = "ltr" | "rtl";
 
